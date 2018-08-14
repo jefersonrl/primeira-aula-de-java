46d707ec505bd0c019920a192582bfeb6af2a9a1
jefersonrl <jefersonrl@icloud.com>
2018-08-13 22:22:17 -0300

    Minha primeira aula de java

---
+++ src/aula/PrimeiraAula.java
@@ -0,0 +1,18 @@
+package aula;
+
+public class PrimeiraAula {
+	public static void main(String args[]) {
+		
+		//variavel do tipo primitivo inteiro
+		int numero = 10, numero2 = 2;
+		
+		/*
+		 * + -> Adicao
+		 * - -> Subtracao
+		 * * -> Multiplicacao
+		 * '/' -> Divisao
+		 * */
+		
+		System.out.print(numero * numero2);
+	}
+}
